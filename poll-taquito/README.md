cc031a836994e330740aaf32735f81674b00f212
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-07-07 11:29:41 +0200

    feat: initialize app and add deployment script

---
+++ poll-taquito/README.md
@@ -0,0 +1,18 @@
+# React-Taquito app to interact with poll smart contract
+
+## Prerequisite
+
+1. Nodejs
+2. Yarn
+
+## Deploying smart contract
+
+1. Create `.env` file using example.
+
+   - `cp .env.example .env`
+   - update the .env file
+
+2. Run: `yarn deploy`
+3. Update the `.env` file with contract address received from above command.
+
+## React App
---
+++ poll-taquito/deployment/deploy.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const { importKey } = require("@taquito/signer");
+const { TezosToolkit, MichelsonMap } = require("@taquito/taquito");
+const pollJSON = require("./poll.json");
+
+const RPC_URL = process.env.RPC_URL;
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const Tezos = new TezosToolkit(RPC_URL);
+
+const deploy = async () => {
+  console.log("Setting Secret key....");
+  await importKey(Tezos, SECRET_KEY);
+  const administrator = await Tezos.signer.publicKeyHash();
+  const initialStorage = {
+    polls: MichelsonMap.fromLiteral({}),
+    votes: MichelsonMap.fromLiteral({}),
+    voters: MichelsonMap.fromLiteral({}),
+    administrator,
+  };
+  console.log("Deploying Contract....");
+  try {
+    const originationOp = await Tezos.contract.originate({
+      code: pollJSON,
+      storage: initialStorage,
+    });
+    console.log(
+      `Waiting for confirmation of origination for ${originationOp.contractAddress}...`
+    );
+    await originationOp.contract();
+    console.log(`Origination completed.`);
+  } catch (error) {
+    console.log(`Error:\n\n${JSON.stringify(error, null, 2)}`);
+  }
+};
+
+deploy();
